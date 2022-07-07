844d69e090f7c1ad034cfdd0def420779e21b12e
Bruno Dalmasso <69864476+brunodalmassomattos@users.noreply.github.com>
2022-07-06 21:19:36 -0300

    Databind eventbind e imperpolacao
    Criacao de um formulario, exportando os dados

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent implements OnInit {
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
 
-  constructor() { }
+  valor: number = 0;
+  destino: number = 0;
 
-  ngOnInit(): void {
+  transferir() {
+    console.log('Transferencia solicitada!');
+
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
   }
 
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
