08c8a7101cf9e379a2cac810322e2e1289cc697e
Bruno Dalmasso <bruno.mdalmasso@gmail.com>
2022-07-08 16:26:51 -0300

    teste

---
+++ crud-angular-spring/crud-angular/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { CursosComponent } from './cursos/cursos/cursos.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'cursos' },
+  {
+    path: 'cursos',
+    loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
